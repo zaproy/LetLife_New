0f70d1d1ac71cbec685e3e192fce416fce467793
TAULT\light_000 <sir.icedteenz@gmail.com>
2017-02-22 22:22:15 +0700

    Add new scene and washNew Script

---
+++ Assets/MiniGame/Script/WashNew.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WashNew : MonoBehaviour {
+
+	public GameObject hygine;
+
+	void Awake(){
+		hygine.SetActive (false);	
+	}
+
+	void OnMouseDown(){
+		
+					Debug.Log ("aaa");
+					hygine.SetActive (true);
+				
+	}
+
+	void OnTriggerEnter2D(Collider2D isDirty){
+		if (isDirty.gameObject.tag == "WashMachine") {
+			Debug.Log ("Dirty");
+		}
+	}
+}
