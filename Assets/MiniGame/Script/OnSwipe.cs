83ac827683a362f013bd11ae16384667c72b99d2
TAULT\light_000 <sir.icedteenz@gmail.com>
2017-02-23 23:26:41 +0700

    Update scene and script onswipe and washmachine

---
+++ Assets/MiniGame/Script/OnSwipe.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnSwipe : MonoBehaviour {
+
+	public enum Swipe
+	{
+		None,Up,Down,Left,Right
+	};
+
+
+	Vector2 firstPress;
+	Vector2 secondPress;
+	Vector2 currentSwipe;
+	public static Swipe swipeDirection;
+	public string direction;
+
+	void Update(){
+		if (swipeDirection == Swipe.Left) {
+			direction = "Left";
+			swipeDirection = Swipe.None;
+		} else {
+			direction = "none";
+		}
+	}
+
+
+
+
+	void OnMouseDown(){
+		firstPress = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
+	}
+
+	void OnMouseUp(){
+		secondPress = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
+		currentSwipe = new Vector2 (secondPress.x - firstPress.x, secondPress.y - firstPress.y);
+
+		currentSwipe.Normalize ();
+
+		if (currentSwipe.y > 0 && currentSwipe.x > -0.2f && currentSwipe.x < 0.2f) {
+			swipeDirection = Swipe.Up;
+		} else if (currentSwipe.y < 0 && currentSwipe.x > -0.2f && currentSwipe.x < 0.2f) {
+			swipeDirection = Swipe.Down;
+		} else if (currentSwipe.x < 0 && currentSwipe.y > -0.2f && currentSwipe.y < 0.2f) {
+			swipeDirection = Swipe.Left;
+		} else if (currentSwipe.x > 0 && currentSwipe.y > -0.2f && currentSwipe.y < 0.2f) {
+			swipeDirection = Swipe.Right;
+		}
+
+	}
+}
--- Assets/MiniGame/Script/Wash_new/WashNew.cs
+++ Assets/MiniGame/Script/Wash_new/WashNew.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WashNew : MonoBehaviour {
-
+	
 	public GameObject hygine;
+
 
 	void Awake(){
 		hygine.SetActive (false);	
 	}
 
+
 	void OnMouseDown(){
 		
-					Debug.Log ("aaa");
-					hygine.SetActive (true);
-				
+		hygine.SetActive (true);
+
 	}
+
+
 
 	void OnTriggerEnter2D(Collider2D isDirty){
 		if (isDirty.gameObject.tag == "WashMachine") {
